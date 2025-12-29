e333c9cd3a2965d8bfc5835f6482c12b9f91c0d1
kushagraswami5 <102528938+kushagraswami5@users.noreply.github.com>
2025-12-29 12:51:52 +0530

    Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://edtech-xdna.onrender.com//api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -68,3 +68,4 @@ export default function Login() {
     </div>
   );
 }
+
